13321ddda8998292b6f51c2ba6645146f2284711
Viacheslav Kaliak <trinidaa@gmial.com>
2025-06-10 16:15:22 +0200

    solution v2

--- datasources.tf
+++ datasources.tf
@@ -1,3 +1,20 @@
 data "azurerm_resource_group" "example" {
   name = "provisioners"
 }
+data "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+data "azurerm_network_interface" "example" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+data "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = data.azurerm_virtual_network.example.name
+}
+data "azurerm_virtual_machine" "example" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = data.azurerm_resource_group.example.name
+}
